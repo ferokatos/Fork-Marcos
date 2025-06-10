f35e853fdd2dfad5d32e247badfb7e7a7b10972e
Gabz Holanda <gabrielholallan09@gmail.com>
2025-06-09 21:23:21 -0300

    Add files via upload

---
+++ forkMarcos.py
@@ -0,0 +1,14 @@
+import os
+import math
+def soma(X,Y):
+    return X + Y
+def sub(X,Y):
+    return X - Y
+def multi(X,Y):
+    return X * Y
+def Div(X,Y):
+    return X / Y
+def raiz(X):
+    return math.sqrt(X)
+def limpa():
+    return os.system('cls')
